3abeb4e83af5f12301f8281a6a90c79660edfdd1
Nicolas <nicolas.programador@gmail.com>
2025-04-22 15:29:59 -0400

    first commit

---
+++ ejercicio1.py
@@ -0,0 +1,35 @@
+primerNombre = "Fernando"
+segundoNombre = "Manuel"
+apellidoP = "Aguirre"
+apellidoM = "Soto"
+rut = "20874524-7"
+ciudad = "Santiago"
+pais = "Chile"
+edad = 25
+altura = 1.78
+
+#print("Su nombre es: " primerNombre + ", su segundo nombre es: " +  segundoNombre + "")
+
+
+
+
+#Numero Entero
+#numero1 = 1
+
+#Flotante
+#numeroFlotante = 3.16
+
+#Cadenas de Texto
+#nombre = "Teodoro"
+
+#aprobado = False
+#conCondena = False
+
+#nombre = input("Ingrese su nombre: ")
+#edad = int(input("Ingrese su edad: "))
+
+    
+#edad = edad + 1
+
+#print("Su nombre es: " + nombre + " y su edad es de: " + edad)
+#print(f"Su nombre es {nombre} y su edad es: {edad}")
